f0668a874ad93a3094d65f2e241bfd0784264803
babafemiolatona <femiolatona@gmail.com>
2024-01-11 23:45:34 +0100

    Fixed error

--- 0x00-python_variable_annotations/8-make_multiplier.py
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 """Complex types - functions"""
+from typing import Callable
 
 
-def make_multiplier(multiplier: float) -> callable:
+def make_multiplier(multiplier: float) -> Callable[[float, float]]:
     """Returns a function that multiplies a float by multiplier."""
     def multiply_by_multiplier(number: float) -> float:
         """Returns the product of number and multiplier."""
