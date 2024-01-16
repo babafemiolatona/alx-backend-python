ada67599168cb46a9363c7de10bd05ef8725dbc4
babafemiolatona <femiolatona@gmail.com>
2024-01-16 17:55:51 +0100

    Update

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -2,9 +2,10 @@
 """Async Generator"""
 import random
 import asyncio
+from typing import Generator
 
 
-async def async_generator():
+async def async_generator() -> Generator[float, None, None]:
     """Async Generator"""
     for _ in range(10):
         await asyncio.sleep(1)
--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -2,12 +2,12 @@
 """Async Comprehensions"""
 import random
 import asyncio
+from typing import Generator
 
 
 async_generator = __import__('0-async_generator').async_generator
 
 
-async def async_comprehension():
+async def async_comprehension() -> Generator[float, None, None]:
     """Async Comprehensions"""
     return [i async for i in async_generator()]
-
