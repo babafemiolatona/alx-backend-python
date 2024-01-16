98f3eb441235f083316e11a4b10688b6c2d9e451
babafemiolatona <femiolatona@gmail.com>
2024-01-16 17:52:51 +0100

    Initial commit

---
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""Async Comprehensions"""
+import random
+import asyncio
+
+
+async_generator = __import__('0-async_generator').async_generator
+
+
+async def async_comprehension():
+    """Async Comprehensions"""
+    return [i async for i in async_generator()]
+
